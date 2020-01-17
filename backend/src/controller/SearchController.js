f46ae0b22c4eacadd7ca1b6bbd11caaba5f598a4
Augusto <augustovds7@gmail.com>
2020-01-16 22:53:37 -0400

    Primeiro Commit

---
+++ backend/src/controller/SearchController.js
@@ -0,0 +1,29 @@
+const Dev = require('../models/Dev');
+
+module.exports = {
+    async index(request, response){
+
+        const { latitude, longitude, techs } = request.query;
+
+        const techsArray = parseStringAsArray(techs);
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+
+            },
+            location:{
+                $near: {
+                    $geometry:{
+                        type: 'Point',
+                        coordinates:  [longitude, latitude],
+                    }
+                }
+            },
+        });
+        // busca todos dev num raio de 10 KM e filtrando por tecnologias
+
+        return response.json({ devs: []});
+
+    }
+}
